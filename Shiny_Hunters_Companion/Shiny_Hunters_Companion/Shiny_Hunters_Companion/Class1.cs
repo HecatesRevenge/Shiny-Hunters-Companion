73ad565d70a0f71deb87a59d6763a36cbd8e9364
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2025-11-13 15:55:22 -0500

    Created class PokemonDB
    Started working on DAL class that allows data objects to access all Pokemon in the DB

--- Shiny_Hunters_Companion/Shiny_Hunters_Companion/Shiny_Hunters_Companion/Class1.cs
+++ Shiny_Hunters_Companion/Shiny_Hunters_Companion/Shiny_Hunters_Companion/Class1.cs
@@ -1,100 +1,39 @@
 ﻿using System;
-using System.IO; // For reading the file
-using System.Data.OleDb; // For talking to Access
-using System.Collections.Generic; // For the Dictionary
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
 
-public class DataImporter
+namespace Shiny_Hunters_Companion
 {
-    private static string _connectionString =
-        @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\YourProject\ShinyCompanion.accdb";
-
-   
-    private static Dictionary<string, int> _pokemonCache = new Dictionary<string, int>();
-
-    public static void RunImport()
+    public class PokemonDB
     {
-        string csvFilePath = @"C:\YourProject\pokemon_import.csv";
+        OleDbConnection myConnection;
+        OleDbDataAdapter myDataAdapter;
+        DataSet myDataSet;
+        BindingSource myBindingSource;
+        string strSql;
 
-     
-        string[] allLines = File.ReadAllLines(csvFilePath);
-
-   
-        for (int i = 1; i < allLines.Length; i++)
+        public PokemonDB()
         {
-            string line = allLines[i];
-            string[] columns = line.Split(',');
 
-     
-            int pokedexNum = int.Parse(columns[0]);
-            string baseName = columns[1];
-            string formName = columns[2];
-            string displayName = columns[3];
-            string spriteURL = columns[4];
-
-            int parentPokemonID = GetOrCreateParentPokemon(baseName, pokedexNum);
-
-  
-            InsertPokemonForm(parentPokemonID, formName, displayName, spriteURL);
-
-            Console.WriteLine($"Successfully imported: {displayName}");
-        }
-        Console.WriteLine("Import Complete!");
-    }
-
-  
-    private static int GetOrCreateParentPokemon(string baseName, int pokedexNum)
-    {
-        if (_pokemonCache.ContainsKey(baseName))
-        {
-            return _pokemonCache[baseName];
-        }
-
-     
-        string sql = "INSERT INTO tblPokemon (PokedexNum, BaseName) VALUES (@PokedexNum, @BaseName);";
-        string sql_identity = "SELECT @@IDENTITY;";
-
-        using (OleDbConnection conn = new OleDbConnection(_connectionString))
-        {
-            conn.Open();
-
-  
-            using (OleDbCommand cmd = new OleDbCommand(sql, conn))
-            {
-                cmd.Parameters.AddWithValue("@PokedexNum", pokedexNum);
-                cmd.Parameters.AddWithValue("@BaseName", baseName);
-                cmd.ExecuteNonQuery();
-            }
-
-            // 2. Get the new ID
-            using (OleDbCommand cmd_id = new OleDbCommand(sql_identity, conn))
-            {
-                int newID = (int)cmd_id.ExecuteScalar();
-
+            myConnection = new OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0;Data Source=ShinyHuntersCompanion.accdb;");
+            //Might need to query different table
+            strSql = "SELECT * FROM tblPokemon";
+            myDataAdapter = new OleDbDataAdapter(strSql, myConnection);
+            myDataSet = new DataSet("AllPokemon");
+            myDataAdapter.Fill(myDataSet, "AllPokemon");
             
-                _pokemonCache.Add(baseName, newID);
-                return newID;
-            }
+            myBindingSource=new BindingSource();
+            myBindingSource.DataSource = myDataSet;
+            myBindingSource.DataMember = "AllPokemon";
+
+           
+
         }
     }
 
-   
-    private static void InsertPokemonForm(int parentID, string formName, string displayName, string spriteURL)
-    {
-        string sql = @"
-            INSERT INTO tblPokemonForms (PokemonID_FK, FormName, DisplayName, SpriteURL) 
-            VALUES (@PokemonID_FK, @FormName, @DisplayName, @SpriteURL)";
-
-        using (OleDbConnection conn = new OleDbConnection(_connectionString))
-        {
-            conn.Open();
-            using (OleDbCommand cmd = new OleDbCommand(sql, conn))
-            {
-                cmd.Parameters.AddWithValue("@PokemonID_FK", parentID);
-                cmd.Parameters.AddWithValue("@FormName", formName);
-                cmd.Parameters.AddWithValue("@DisplayName", displayName);
-                cmd.Parameters.AddWithValue("@SpriteURL", spriteURL);
-                cmd.ExecuteNonQuery();
-            }
-        }
-    }
 }
