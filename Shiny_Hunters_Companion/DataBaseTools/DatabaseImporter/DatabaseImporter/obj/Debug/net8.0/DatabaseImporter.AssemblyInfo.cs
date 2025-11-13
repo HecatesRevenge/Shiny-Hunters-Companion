8f908343eb2c8ffcd1b25b3e73aee84ea0a9f525
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2025-11-13 00:52:10 -0500

    Final Database Config
    Following Tables following configured: tblPokemon and tblPokemonForms
    
    Added console script to check the url of the images

--- Shiny_Hunters_Companion/DataBaseTools/DatabaseImporter/DatabaseImporter/obj/Debug/net8.0/DatabaseImporter.AssemblyInfo.cs
+++ Shiny_Hunters_Companion/DataBaseTools/DatabaseImporter/DatabaseImporter/obj/Debug/net8.0/DatabaseImporter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseImporter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bff03081f03254163d620486acb85847536279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a77eec63381fcb768cccab8c7fd759d89d3650b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseImporter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseImporter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
