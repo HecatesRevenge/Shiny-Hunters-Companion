6311b177cefbf4724b2928ddc463eb689f79864f
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2025-11-20 16:35:28 -0500

    Created classes GameDB and Game
    Create the data object Game.cs and DAL object GameDB. Still need to include more methods in GameDB but logic is there.

---
+++ Shiny_Hunters_Companion/Shiny_Hunters_Companion/Shiny_Hunters_Companion/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiny_Hunters_Companion
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public string GameName { get; set; }
+        public string Generation { get; set; }
+        public string Platform { get; set; }
+        public string RegionName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string BoxArtURL { get; set; }
+
+        public Game() { }
+
+        public Game(
+            int gameID, 
+            string gameName, 
+            string generation, 
+            string platform, 
+            string regionName, 
+            DateTime releaseDate, 
+            string boxArtURL)
+        {
+            GameID = gameID;
+            GameName = gameName;
+            Generation = generation;
+            Platform= platform;
+            RegionName = regionName;
+            ReleaseDate = releaseDate;
+            BoxArtURL = boxArtURL;
+        }
+
+        public override string ToString()
+        {
+            //TODO: Adjust what is displayed if need be
+            return GameName; 
+        }
+    }
+}
---
+++ Shiny_Hunters_Companion/Shiny_Hunters_Companion/Shiny_Hunters_Companion/GameDB.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Shiny_Hunters_Companion
+{
+    public class GameDB
+    {
+        private string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ShinyCompanion.accdb;";
+        private OleDbConnection myConnection;
+
+        public GameDB()
+        {
+            myConnection = new OleDbConnection(connectionString);
+
+        }
+
+        private Game GetGameFromReader(OleDbDataReader reader)
+        {
+            return new Game
+            {
+                GameID = Convert.ToInt32(reader["GameID"]),
+                GameName = reader["GameName"].ToString(),
+                Generation = reader["Generation"].ToString(),
+                Platform = reader["platform"].ToString(),
+                RegionName = reader["RegionName"].ToString(),
+                ReleaseDate = Convert.ToDateTime(reader["ReleaseDate"]),
+                BoxArtURL = reader["BoxArtURL"].ToString()
+
+            };
+        
+
+        }
+
+        private List<Game> DatabaseSelectQuery(string query, Dictionary<string, object> parameters = null)
+        {
+            List<Game> results = new List<Game>();
+            try
+            {
+                if (myConnection.State != ConnectionState.Open)
+                {
+                    myConnection.Open();
+                }
+                using (OleDbCommand command = new OleDbCommand(query, myConnection))
+                {
+                    if (parameters != null)
+                    {
+                        foreach (var p in parameters)
+                        {
+                            command.Parameters.AddWithValue(p.Key, p.Value);
+                        }
+                    }
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            results.Add(GetGameFromReader(reader));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database error: " + ex.Message);
+            }
+            finally
+            {
+                if (myConnection.State == ConnectionState.Open)
+                {
+                    myConnection.Close();
+                }
+            }
+            return results;
+        }
+
+        public List<Game> GetAllGames()
+        {
+            string strSQL = "SELECT * FROM Games ORDER BY ReleaseData";
+            return DatabaseSelectQuery(strSQL);
+        }
+
+
+
+    }
+
+}
