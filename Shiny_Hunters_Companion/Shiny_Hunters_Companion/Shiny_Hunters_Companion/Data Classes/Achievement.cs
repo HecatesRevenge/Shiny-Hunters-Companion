463d50643987075059f8ac784ff8b4dc03c2fc93
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2025-11-22 23:24:36 -0500

    Working on AchievmentsDB

---
+++ Shiny_Hunters_Companion/Shiny_Hunters_Companion/Shiny_Hunters_Companion/Data Classes/Achievement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiny_Hunters_Companion
+{
+    public class Achievement
+    {
+        public int AchievementID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ConditionType { get; set; }
+        public int Threshold { get; set; }
+        public string ImageURL { get; set; }
+        public bool IsUnlocked { get; set; }
+        public DateTime UnlockDate { get; set; }
+        public Achievement() { }
+    }
+}
