6a41f44b869d7f92b525d7a7a900b2aae0926567
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2025-12-01 20:07:47 -0500

    Working on ShinyPokedex Form

---
+++ Shiny_Hunters_Companion/Shiny_Hunters_Companion/Shiny_Hunters_Companion/Interface/ShinyPokedex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shiny_Hunters_Companion
+{
+    public partial class ShinyPokedex : Form
+    {
+        public ShinyPokedex()
+        {
+            InitializeComponent();
+        }
+
+        private void ShinyPokedex_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
